83ece3049283c791d88e9eaa29c4e9e871543e3c
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-09-09 16:24:54 +0200

    Create categories.js

---
+++ src/data/categories.js
@@ -0,0 +1,13 @@
+export default [
+    'smartphones',
+    'laptops',
+    'skincare',
+    'groceries',
+    'home-decoration',
+    'furniture',
+    'tops',
+    'women-dresses',
+    'women-shoes',
+    'men-shirts',
+    'men-shoes',
+]
