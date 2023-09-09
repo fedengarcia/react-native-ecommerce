42ac885479a37032f6363261621f81858d38fc59
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-09-09 18:44:22 +0200

    styles product screen

--- src/screens/Products/Products.jsx
+++ src/screens/Products/Products.jsx
@@ -1,13 +1,43 @@
-import { Text, View } from 'react-native'
-import React from 'react'
-import { Header } from '../../components'
+import { FlatListComponent, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Header, SearchInput } from '../../components'
+import products from '../../data/products'
+import styles from './ProductsStyle'
 
-const Products = ({product}) => {
+
+const Products = ({categorySelected}) => {
+
+  const [productsList, setProductsList] = useState([]);
+  const [keyword, setKeyword] = useState('');
+
+  useEffect(() => {
+    let productsFiltered = []
+    if(categorySelected){
+      const productsSelected = products.filter(prod => prod.category === categorySelected);
+      productsFiltered = productsSelected.filter(prod => prod.title.includes(keyword));
+    }else{
+      productsFiltered = products.filter(prod => prod.title.includes(keyword));
+    }
+    setProductsList(productsFiltered);
+
+  }, [categorySelected, keyword]);
+
+
   return (
-    <>
-      <Header title={product}/>
-        <Text>Details</Text>
-    </>
+    <View style={styles.container}>
+      <Header title={"Products"}/>
+      <SearchInput
+        onSearch={setKeyword}
+      />
+      <View style={styles.productsListContainer}>
+        <FlatListComponent
+          data={productsList}
+          renderItem={() => <View>{item.title}</View>}
+          keyExtractor={item => item.id}
+        />
+        
+      </View>
+    </View>
   )
 }
 
--- src/screens/Products/ProductsStyle.js
+++ src/screens/Products/ProductsStyle.js
@@ -1,8 +1,15 @@
 import { StyleSheet } from "react-native";
+import { COLORS } from "../../global/COLORS";
 
 export const styles = StyleSheet.create({
     container:{
         flex: 1,
-        width: '100%'
+        width: '100%',
+        backgroundColor: COLORS.primary,
+    },
+    productsListContainer:{
+        paddingHorizontal: 15,
+        flex: 1,
+        marginTop: 15
     }
 })
