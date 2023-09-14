ecebb0de6c33df14af67a22c62321252d5ab8b1e
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-09-14 09:00:51 +0200

    use fonts

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { Home } from './src/screens';
 import { Products } from './src/screens';
+import { useState } from 'react';
+import { useFonts } from 'expo-font';
+import fonts from './src/global/fonts';
 
 export default function App() {
+  const [categorySelected,setCategorySelected] = useState('')
+  const [fontsLoaded] = useFonts(fonts);
+  
+  if(!fontsLoaded) return null;
+
   return (
     <View style={styles.container}>
-      {/* <Home/> */}
-      <Products categorySelected={"smartphones"}/>
+      {categorySelected ?
+          <Products categorySelected={categorySelected}/> 
+        : 
+          <Home setCategorySelected={setCategorySelected}/>
+      }
     </View>
   );
 }
