9170d8e20774905d1d0cd5949084a1adaf57b3a2
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-09-23 15:18:36 +0200

    finish counterSlice

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "../features/counter/counterSlice";
+
 
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        counter: counterSlice
+    },
 })
