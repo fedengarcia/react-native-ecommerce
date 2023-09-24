3d2fc1c89926f5dfa981697b058b24611626193d
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-09-24 13:54:02 +0200

    update home

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,21 +1,50 @@
-import { FlatList, Text, View } from 'react-native'
-import React from 'react';
-import { styles } from './HomeStyles';
-import { CategoryItem, Header } from '../../components';
-import { DATA_CATEGORIES } from '../../data/global_data';
+import { FlatList, Text, TouchableOpacity, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Card, Header, SearchInput } from '../../components'
+import { styles } from './HomeStyles'
+import { DATA_PRODUCTS } from '../../data/products'
+import ProductItem from '../../components/ProductsComponents/ProductItem/ProductItem'
 
 
 const Home = ({navigation}) => {
+  const [productsList, setProductsList] = useState([]);
+  const [categorySelected, setCategorySelected] = useState('all');
+  const [keyword, setKeyword] = useState('');
+  console.log("YOUR CATEGORY")
+
+  useEffect(() => {
+    let productsFiltered = []
+    if(categorySelected !== 'all'){
+      const productsSelected = DATA_PRODUCTS.filter(prod => prod.category === categorySelected);
+      productsFiltered = productsSelected.filter(prod => prod.title.includes(keyword));
+    }else{
+      productsFiltered = DATA_PRODUCTS.filter(prod => prod.title.includes(keyword));
+    }
+    setProductsList(productsFiltered);
+
+  }, [keyword]);
+
   return (
     <View style={styles.container}>
-      <Header title={"Categories"}/>
-      <View style={styles.categoriesContainer}>
-          <FlatList
-              style={styles.flatList}
-              data={DATA_CATEGORIES}
-              keyExtractor={category => category}
-              renderItem={({item}) => <CategoryItem navigation={navigation} categorySelected={item}/>}
-              />
+      <Header title={"Home"} navigation={navigation}/>
+      <SearchInput
+        onSearch={setKeyword}
+      />
+      <View style={styles.productsListContainer}>
+        {productsList.length > 0 ?
+        <FlatList
+          style={styles.flatList}
+          numColumns={2}
+          horizontal={false}
+          data={productsList}
+          keyExtractor={this.keyExtractor}
+          renderItem={({item}) => 
+          <TouchableOpacity style={styles.itemContainer} onPress={() => navigation.navigate("Details", {product: item})}>
+            <ProductItem product={item}/>
+          </TouchableOpacity>}
+        />:
+          <Text>No hay productos para mostrar</Text>
+        }
       </View>
     </View>
   )
--- src/screens/Home/HomeStyles.js
+++ src/screens/Home/HomeStyles.js
@@ -2,20 +2,22 @@ import { StyleSheet } from "react-native";
 import { COLORS } from "../../global/COLORS";
 
 export const styles = StyleSheet.create({
-    container: {
+    container:{
+        flex: 1,
         width: '100%',
-        height:'100%',
-        backgroundColor: COLORS.backgroundGray1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor: COLORS.primary,
+        alignItems:'center'
     },
-    categoriesContainer: {
-        width: '100%',
-        height:'90%',
-        justifyContent: 'center',
-        alignItems: 'center',
+    productsListContainer:{
+        width:  '100%',
+        paddingHorizontal: 10,
+        marginTop: 15,
     },
-    flatList:{
+    flatList: {
         width: '100%',
+    },
+    itemContainer:{
+        width:'50%',
     }
+
 })
