d6bae15b423ebcc534d23bf3efe0184a48ea91b3
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-09-24 14:24:57 +0200

    Update CategoriesLIst.jsx

--- src/screens/CategoriesList/CategoriesLIst.jsx
+++ src/screens/CategoriesList/CategoriesLIst.jsx
@@ -1,7 +1,8 @@
-import { Text, View } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
 import React from 'react'
 import { CategoryItem, Header } from '../../components'
 import {styles} from "./CategoriesListStyle"
+import {DATA_CATEGORIES} from '../../data/global_data';
 
 const CategoriesList = ({navigation}) => {
   return (
