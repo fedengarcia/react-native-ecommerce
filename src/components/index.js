ae59292e1357923e814d0d371d49fcbb13e2cafc
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-09-24 14:25:20 +0200

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Header from "./Header/Header";
 import SearchInput from './SearchInput/SearchInput';
 import CategoryItem from './HomeComponents/CategoryItem/CategoryItem';
 import CartItem from './CartComponents/CartItem/CartItem';
+import Filters from "./HomeComponents/Filters/Filters";
 
-export {Card, Header, SearchInput, CartItem, CategoryItem};
+export {Card, Header, SearchInput, CartItem, CategoryItem, Filters};
