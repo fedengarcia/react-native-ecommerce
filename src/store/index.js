526fa0e5f8a8e4b6f1ea6e27a3823ea54782ce1b
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-10-02 18:58:52 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import shopSlice from "../features/shop/shopSlice";
 import { shopApi } from "../services/shopAPI";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import cartSlice from "../features/cart/cartSlice";
 
 const store = configureStore({
     reducer: {
         shop: shopSlice,
+        cart: cartSlice,
         [shopApi.reducerPath]: shopApi.reducer
     },
     middleware: getDefaultMiddleware => getDefaultMiddleware().concat(shopApi.middleware)
