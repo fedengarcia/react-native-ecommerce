0d0ece0bf8b07271e6b328b441bdcb98ef7a0268
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-10-02 18:58:50 +0200

    Create cartSlice.js

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,47 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    user: "user",
+    updatedAt: Date.now().toLocaleString(),
+    items: [],
+    total: null
+}
+
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addItem: (state, action) => {
+            const productRepeated = state.items.find(item => item.id === action.payload.id);
+            if(productRepeated){
+                const itemsUpdated = state.items.map(item => {
+                    if(item.id === action.payload.id){
+                        item.quantity += action.payload.quantity;
+                        return item;
+                    }
+                    return;
+                }) 
+                const total = itemsUpdated.reduce((acc, current) => (acc += current.price * current.quantity));
+                state = {
+                    ...state,
+                    items: itemsUpdated,
+                    total,
+                    updatedAt: new Date().toDateString()
+                }
+            }else{
+                state.items.push(action.payload);
+                const total = state.items.reduce((acc,current) => (acc += current.price * current.quantity));
+                state = {
+                    ...state,
+                    total,
+                    updatedAt: new Date().toDateString()
+                }
+            }
+        },
+        removeItem: (state,action) => {}
+    }
+})
+
+export const { addItem, removeItem} = cartSlice.actions;
+
+export default cartSlice.reducer
