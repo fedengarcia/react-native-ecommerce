eccdc75a840142182ee391809642c604a86202fb
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-10-03 13:51:02 +0200

    Update index.js

--- src/screens/index.js
+++ src/screens/index.js
@@ -3,5 +3,7 @@ import Details from "./Details/Details";
 import Cart from "./Cart/Cart";
 import Orders from "./Orders/Orders";
 import CategoriesList from './CategoriesList/CategoriesLIst';
+import Login from './Login/Login';
+import SingUp from './SingUp/SingUp';
 
-export {Home, Details, Cart, Orders, CategoriesList}
+export {Home, Details, Cart, Orders, CategoriesList, Login, SingUp}
