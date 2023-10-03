8adeb6b783503cd9182641a4947d3736b0508f05
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-10-03 13:51:09 +0200

    create login

---
+++ src/screens/Login/Login.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {FlatList, Pressable, Text, TextInput, TouchableOpacity, View} from 'react-native';
+import {styles} from './LoginStyles';
+import { Card, Header } from '../../components';
+
+
+const Login = ({navigation}) => {
+    return (
+        <View style={styles.container}>
+            {/* <Header title={'Login'} navigation={navigation}/> */}
+            <View style={styles.loginContainer}>
+              <Card style={styles.loginCard}>
+                <Text style={styles.title}>Login to start</Text>
+                <TextInput style={styles.input}/>
+                <TextInput style={styles.input}/>
+                <TouchableOpacity style={styles.button}>
+                  <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+                  <Text style={styles.notAccountText}>No have an account?</Text>
+                  <TouchableOpacity  style={styles.button}>
+                    <Text style={styles.buttonText}>Sing up</Text>
+                  </TouchableOpacity>
+              </Card>
+            </View>
+           
+        </View>
+    );
+}
+
+
+export default Login;
---
+++ src/screens/Login/LoginStyles.js
@@ -0,0 +1,56 @@
+import { StyleSheet } from "react-native";
+import { COLORS } from "../../global/COLORS";
+
+export const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        width: '100%',
+        backgroundColor: COLORS.primary,
+        alignItems:'center',
+        justifyContent:'center',
+        padding: 20,
+        paddingTop: 50
+    },
+    loginContainer: {
+        flex:1,
+        width: '100%',
+        height:'100%',
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    loginCard: {
+        alignItems:'center',
+        flex: 0,
+        height:'auto',
+        width: '70%',
+        padding: 50,
+        borderRadius: 20,
+    },
+    title: {
+        color: COLORS.primary,
+        fontSize: 60,
+    },
+    input: {
+        borderRadius: 20,
+        backgroundColor: COLORS.backgroundGray2,
+        width: '100%',
+        height: 80,
+        marginVertical: 20,
+    },
+    notAccountText:{
+        marginTop: 40,
+        fontSize: 40,
+    },
+    button: {
+        borderRadius: 20,
+        padding: 20,
+        backgroundColor: COLORS.secondary,
+        width: '50%',
+        alignSelf: 'center',
+    },
+    buttonText:{
+        alignSelf: 'center',
+        fontSize: 40,
+        color: COLORS.primary
+    }
+})
