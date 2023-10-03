a9e27c66693dfde83059cdf5f9ee8132008db698
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-10-03 13:52:28 +0200

    Update SingUp.jsx

--- src/screens/SingUp/SingUp.jsx
+++ src/screens/SingUp/SingUp.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import {FlatList, Pressable, Text, TextInput, TouchableOpacity, View} from 'react-native';
 import {styles} from './SingUpStyles';
-import { Card, Header } from '../../components';
+import { Card } from '../../components';
 
 
 const SingUp = ({navigation}) => {
     return (
         <View style={styles.container}>
-            {/* <Header title={'Login'} navigation={navigation}/> */}
             <View style={styles.loginContainer}>
               <Card style={styles.loginCard}>
-                <Text style={styles.title}>Create an account</Text>
+                <Text style={styles.title}>Register to start</Text>
                 <TextInput style={styles.input}/>
                 <TextInput style={styles.input}/>
                 <TextInput style={styles.input}/>
                 <TextInput style={styles.input}/>
+                <TouchableOpacity style={styles.button}>
+                  <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+                  <Text style={styles.notAccountText}>Already have an account?</Text>
                   <TouchableOpacity  style={styles.button}>
-                    <Text style={styles.buttonText}>Sing up</Text>
+                    <Text style={styles.buttonText}>Login</Text>
                   </TouchableOpacity>
               </Card>
             </View>
