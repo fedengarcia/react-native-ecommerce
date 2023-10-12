135303264fa5a3c5431a90447180eb099ecfefc6
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2023-10-12 15:40:17 +0200

    fetch session

--- src/db/index.js
+++ src/db/index.js
@@ -5,17 +5,15 @@ const db = SQLite.openDatabase('sessions.db');
 
 // This db is created in the local memory of the phone
 export const initDB = () => {
-    const promise =  new Promise(({resolve, reject}) => {
+    const promise =  new Promise((resolve, reject) => {
         db.transaction(tx => {
             tx.executeSql('CREATE TABLE IF NOT EXISTS sessions (localId TEXT PRIMARY KEY NOT NULL, email TEXT NOT NULL, token TEXT NOT NULL)',
-            [],
-            ()=> resolve(),
-            (_, error) => {
-                reject(error);
-            })
+                [],
+                (_, result)=> resolve(result),
+                (_, error) => reject(error)
+            )
         })
     })
-
     return promise;
 }
 
@@ -23,11 +21,26 @@ export const initDB = () => {
 export const insertNewSession = ({localId, email, token}) => {
     const promise = new Promise((resolve, reject) => {
         db.transaction(tx => {
-            tx.executeSql('INSERT INTO sessions (localId, email, token) (?,?,?);',
-            [localId, email, token]),
-            (_, result) => resolve(result),
-            (_, error) => reject(error)
+            tx.executeSql('INSERT INTO sessions (localId, email, token) VALUES (?,?,?)',
+                [localId, email, token],
+                (_, result) => resolve(result),
+                (_, error) => reject(error)
+            )
         })
     })
     return promise;
+}
+
+
+export const fetchSession = () => {
+    const promise = new Promise((resolve, reject) => {
+        db.transaction(tx => {
+            tx.executeSql('SELECT * FROM sessions', 
+                [],
+                (_,result) => resolve(result),
+                (_,error) => reject(error)
+            )
+        })
+    })
+    return promise
 }
